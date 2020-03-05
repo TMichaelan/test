e5bb1f3e30b8f8671ae0245bf59facfa9223a372
Michael <bigapplematthias@gmail.com>
2020-03-05 19:04:38 +0100

    Selection Sort

--- 1.py
+++ 1.py
@@ -1,3 +1,10 @@
-print('Hello World1')
-print('keklol')
-print('helloWorld2.0')
+#Selection Sort
+A = [9,4,5,7,1,2,4,6,8,9,10,3,2,7,8,9,0]
+
+for i in range(len(A)):
+    min_idx = i
+    for j in range(i + 1, len(A)):
+        if A[min_idx] > A[j]:
+            min_idx = j
+    A[i], A[min_idx] = A[min_idx], A[i]
+print(A)
