8d07d11eff9a6caf563eb42ca96953c889fd64e6
Michael <bigapplematthias@gmail.com>
2020-03-05 18:03:33 +0100

    version 2

--- 1.py
+++ 1.py
@@ -1,17 +1,2 @@
-# from kivy.app import App
-# from kivy.uix.button import Button
-# from kivy.uix.widget import Widget
-# from kivy.uix.label import Label
-# from kivy.uix.gridlayout import GridLayout
-# from kivy.uix.boxlayout import BoxLayout
-# from kivy.config import Config
-# #
-#
-# Config.set('graphics','reziable',0)
-# Config.set('graphics','width',400)
-# Config.set('graphics','height',500)
-
-
 print('Hello World1')
-
 print('keklol')
