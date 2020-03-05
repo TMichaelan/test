b862ec9e54f218d617026d13f3aa81b33b083e65
Michael <bigapplematthias@gmail.com>
2020-03-05 19:06:32 +0100

    Selection Sort + Reversed

--- 1.py
+++ 1.py
@@ -8,3 +8,14 @@ for i in range(len(A)):
             min_idx = j
     A[i], A[min_idx] = A[min_idx], A[i]
 print(A)
+
+#Reversed
+array = [9,4,5,7,1,2,4,6,8,9,10,3,2,7,8,9,0]
+
+for i in range(len(array)):
+    min_index = i
+    for j in range(i+1,len(array)):
+        if array[min_index] < array[j]:
+            min_index = j
+    array[i],array[min_index] = array[min_index],array[i]
+print(array)
